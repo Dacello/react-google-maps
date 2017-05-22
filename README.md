b52a6de60a119d02a447918350cf08b455225585
Daniel Andrews <daniel@revelry.co>
2017-05-22 16:49:18 -0500

    Simple google maps component!

---
+++ README.md
@@ -0,0 +1,9 @@
+**React with Google Maps**
+
+*A Repo to provide a working example to go with the Blog Post*
+http://revelry.co/google-maps-react-component-in-es6
+
+`npm install`
+`npm run dev`
+
+Open up `src/client/app/index.html` in your browser and you should see the map!
---
+++ src/client/app/index.jsx
@@ -0,0 +1,20 @@
+import css from './stylesheets/base.css';
+
+import React from 'react';
+import {render} from 'react-dom';
+
+import GMap from './components/GMap.jsx';
+
+class App extends React.Component {
+  get initialCenter() {
+    return { lng: -90.1056957, lat: 29.9717272 }
+  }
+  render () {
+    return <div>
+      <h1>React with Google Maps</h1>
+      <GMap initialCenter={this.initialCenter} />
+    </div>;
+  }
+}
+
+render(<App/>, document.getElementById('ReactGMapsApp'));
