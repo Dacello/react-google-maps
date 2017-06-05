5ed40e0a3ef8febec12dd88cc755bb8b9e62bff8
CJ Horton <thecjhorton@gmail.com>
2017-06-05 12:58:37 -0500

    added/configured webpack-dotenv-plugin to avoid committing API key

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var webpack = require('webpack');
 var path = require('path');
+var DotenvPlugin = require('webpack-dotenv-plugin');
 
 var BUILD_DIR = path.resolve(__dirname, 'public');
 var APP_DIR = path.resolve(__dirname, 'app');
@@ -19,7 +20,13 @@ var config = {
         loader : 'babel-loader'
       }
     ]
-  }
+  },
+  plugins: [
+    new DotenvPlugin({
+      sample: './.env.example',
+      path: './.env'
+    })
+  ]
 };
 
 module.exports = config;
